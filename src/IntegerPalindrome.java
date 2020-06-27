e441876a7760e366ad8eeb52c4d18870be775300
upretim <manojupreti83@yahoo.co.in>
2020-06-27 17:24:29 +0530

    Palindrome Number

---
+++ src/IntegerPalindrome.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntegerPalindrome {
+    /*
+    * This is generally asked as follow-up or alternative of the previous program.
+    *  This time you need to check if given Integer is palindrome or not.
+    * An integer is called palindrome if it's equal to its reverse, e.g. 1001 is a palindrome,
+    * but 1234 is not because the reverse of 1234 is 4321 which is not equal to 1234.
+
+*/
+    public static void main(String[] args) {
+        System.out.println(isNumberPalindrome(27122));
+        System.out.println(isNumberPalindrome(123321));
+        System.out.println(isNumberPalindrome(12321));
+    }
+    private static boolean isNumberPalindrome(int a){
+       List<Integer> arr = new ArrayList<>();
+       int digit;
+       if (a<10) return true;
+       while(a>0){
+           digit = a%10;
+           a= a /10;
+           arr.add(digit);
+       }
+       int limit = arr.size()/2;
+       int j = arr.size() -1;
+      for(int i=0;i<limit;i++){
+          if(arr.get(i)==arr.get(j)){
+              j--;
+              continue;
+          }
+          return false;
+      }
+        return true;
+    }
+}
+
